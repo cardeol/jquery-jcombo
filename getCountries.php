b055a1f1699509fbc04396c4365e3e40b794a2b1
cardeol <cardeol@gmail.com>
2013-12-23 12:30:50 +0000

    Update getCountries.php
    updated new version

--- getCountries.php
+++ getCountries.php
@@ -8,18 +8,18 @@
     if($result && 
        mysql_num_rows($result)>0) {
         while($row = mysql_fetch_array($result)) {
-            $items[$row[0]] = $row[1];
+            $items[] = array("id" => $row[0], "value" => $row[1]);
         }        
     }
     mysql_close();	
 	header('content-type: application/json; charset=utf-8');
-    // convert into JSON format and print
+	// convert into JSON format and print
 	$response = json_encode($items);
 	if(isset($_GET['callback'])) {
 		echo $_GET['callback']."(".$response.")";  
 	} else {
 		echo $response;			
 	}
-    
+
 	
 ?>
