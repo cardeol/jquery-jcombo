13019f9655765a688407b509b934dd54bb0cf615
cardeol <cardeol@gmail.com>
2013-12-23 23:45:54 +0000

    Update jquery.jcombo.js
    updated, data elements removed, input parameters by url, post or get, reduced script size

--- jquery.jcombo.js
+++ jquery.jcombo.js
@@ -1,111 +1,128 @@
 ;(function ( $, window, document, undefined ) {
     var pluginName = "jCombo",
+		version = "3.0",
         defaults = {   
             url: null,         
             parent: null,
             first_optval : "__jc__",
+            input_param: null,
+            data: null,
             selected_value : null,
             initial_text: "-- Please Select --",            
             method: "GET",
             dataType: "jsonp"
-        },
-        version = "2.0";
+        };        
     function Plugin( element, options ) {
-        this.options = $.extend( {}, defaults, options) ;
+        this.options = $.extend( {}, defaults, options) ;        
         this.element = element;
         this.init();                        
     };    
-    Plugin.prototype = {      
+    Plugin.prototype = {               
+        _createCache: function( requestFunction ) {
+            var self = this;
+            self.cache = {};
+            return function( key, callback ) {
+                if ( !self.cache[ key ] ) {
+                    self.cache[ key ] = $.Deferred(function( defer ) {
+                        requestFunction( defer, key );
+                    }).promise();
+                }
+                return self.cache[ key ].done( callback );
+            };
+        },
+        _addParameter: function(param,value) {
+            var xdata = "";
+            if(param==null || value == null) return false;
+            this.options.data = param + "=" + value;            
+			return true;
+        },
+        _getJson: function(url,cback) {
+            var self = this;           
+            var mydata = (this.options.orig_data == null) ? "" : this.options.orig_data + "&";
+            $.jCall = this._createCache(function( defer, url ) {
+                $.ajax({
+                    url: url,
+                    data: mydata + self.options.data,
+                    type: self.options.dataType,                
+                    dataType: "jsonp",
+					method: self.options.method,
+                    success: defer.resolve,
+                    error: defer.reject
+                });
+            });            
+            $.jCall(url,cback);
+        },
         _getData: function(url,cback) {
             var self = this;   
             window.__jcombo_data_cache = (typeof window.__jcombo_data_cache === "undefined") ? {} : window.__jcombo_data_cache;
-            var cK = JSON.stringify(url);  
-            if (!window.__jcombo_data_cache[cK]) {
-                $.ajax({
-                    type: self.options.method,
-                    dataType: self.options.dataType,                    
-                    url: url,
-                    success: function(data) {
-                        window.__jcombo_data_cache[cK] = data;   
-                        cback(data);
-                    }
-                });                 
+            var cK = JSON.stringify(url + self.options.orig_data + self.options.data);  
+            if (!window.__jcombo_data_cache[cK] ) {
+                self._getJson(url,function(data) {
+                    window.__jcombo_data_cache[cK] = data;   
+                    cback(data);
+                });                
             } else setTimeout(function() { cback(window.__jcombo_data_cache[cK]); },0);
+        },
+        _renderOption: function(v,t,s) {
+            var sel = "";
+            if(s==true) sel = ' selected="selected"';
+            return '<option value="' + v + '"' + sel + '>' + t + '</option>';
         },
         _firstOption: function() {
             if(this.initial_text == "") return "";
-            return '<option value="' + this.options.first_optval + '">' + this.options.initial_text + '</option>';
+            return this._renderOption(this.options.first_optval,this.options.initial_text,false);
         },
         _renderSelect: function(data, selected_value) {
             var response = [];
             if(typeof selected_value == "undefined") selected_value = this.options.first_optval;
             response.push(this._firstOption());
-            var selected;
             for(var index in data) {
-                selected = (index == selected_value)?' selected="selected"':'';
-                response.push('<option value="' + index + '"' + selected + '>' + data[index] + '</option>');
+                var option = data[index];
+                response.push(this._renderOption(option.id,option.value,option.id == selected_value));                
             }
             return response.join("");
-        },        
+        },                
         init: function() {                      
-            var self = this;
-            var parent_selected = null;
-            if(this.options.selected_value!=null) {
-                $(self.element).data("jcombo-selected",this.options.selected_value);                
-            } 
-
-            if(this.options.parent!=null) {
-                $(this.options.parent).each(function(index,elem) {
-                    var psel = $(elem).data("jcombo-selected");
-                    if(!(typeof psel === "undefined")) parent_selected = psel;
-                    return false;
-                });
-            }            
+            var self = this;            
+            this.options.orig_data = (this.options.data == null) ? "" : this.options.data;
+            var parent_selected = null;            
             if(this.options.url!=null) {     
-                $(this.element).data("jcombo-url",this.options.url);
                 var turl = this.options.url;
-                if(this.options.parent!=null) turl+=parent_selected;
+                if(this.options.parent!=null) {
+                    if(this.options.input_param==null) turl+=parent_selected;                          
+                    else this._addParameter(this.options.input_param,parent_selected);
+                } 
                 this._getData(turl ,function(data) {                                                         
-                    var inner = self._renderSelect(data,self.options.selected_value);
-                    $(self.element).html(inner);
+                    $(self.element).html(self._renderSelect(data,self.options.selected_value));
                 });                                    
             }           
             if(this.options.parent!=null) {
                 var parents = this.options.parent;
-                $(parents).each(function(index,elem) {                          
-                    var cselec = $(elem).data("jcombo-children");                    
-                    if(!(cselec instanceof Array)) cselec = [];
-                    if(cselec.indexOf(self.options.selector)<0) {
-                        cselec.push(self.options.selector);
-                    }                    
-                    $(elem).data("jcombo-children",cselec);
-                    $(elem).bind("change",function() {
-                        var xsel =$(elem).data("jcombo-children");
-                        var value = $(elem).val();
-                        for(k in xsel) {                                                        
-                            var msel = xsel[k];
-                            if(value == self.options.first_optval) {
-                                $(msel).html(self._firstOption());
-                                $(msel).attr("disabled","disabled");
-                            } else {           
-                                $(msel).removeAttr("disabled");      
-                                $(msel).each(function(index,son) {
-                                    var url = $(son).data("jcombo-url");
-                                    self._getData(url + value,function(data) {
-                                        var inner = self._renderSelect(data);
-                                        $(son).html(inner);
-                                        $(son).trigger("change");
-                                    });
-                                });
-                            };                            
-                        };                        
-                    });                      
+                $(parents).each(function(index,elem) {                                              
+                     $(elem).bind("change",function() {
+                          var value = $(elem).val();
+						  if(value == self.options.first_optval) {
+                                $(self.element).html(self._firstOption());
+                                $(self.element).attr("disabled","disabled");
+                          } else { 
+						  	 	$(self.element).removeAttr("disabled");
+								 if(self.options.input_param!=null) {
+									self._addParameter(self.options.input_param,value);
+									value = '';									
+							    }
+						  		self._getData(self.options.url + value, function(data) {                                
+								   $(self.element).html(self._renderSelect(data));
+								   $(self.element).trigger("change");
+							  });
+						  }
+						  
+                     });
+
                 });
             };           
         }        
     };
     $.fn[pluginName] = function ( options ) {
-        options.selector = $(this).selector;
         return this.each(function () {
             if (!$.data(this, "plugin_" + pluginName)) {
                 $.data(this, "plugin_" + pluginName,
